d857bbd346cf99eed3a29f4bb935e8ce99497fa3
Morten Bendiksen <morten.bendiksen@gmail.com>
2015-08-09 22:29:13 +0200

    refactor

--- src/mandel/core.clj
+++ src/mandel/core.clj
@@ -5,7 +5,7 @@
    (:import [mandel.complex complex]))
 
 
-(defn mandel [c]
+(defn mandel [^complex c]
   (let [end 1000]
     (loop [z (complex. 0 0)
            n 0]
@@ -16,49 +16,42 @@
                  (+ n 1)))
         (double -1.0)))))
 
-(defn setup [])
 
-(def wo 0.0000001)
-(def ox 0.2694911)
-(def oy -0.004556107)
 
-(defn set-color [^double m index ^ints pxls]
-  (if (< m 0)
-    (aset pxls index (^int q/color 0 0 0))
-    (aset pxls index (^int q/color (* m 255) (* m 128) 200))))
+(def viewwidth 0.0000001)
+(def centerx 0.2694911)
+(def centery -0.004556107)
+
 
 (defrecord point [^double x ^double y])
 
 (defn draw []
-   (time
-    (let [
-            ^ints pxls (q/pixels)
+     (let [^ints image (q/pixels)
             width (q/width)
             height (q/height)
-            left (- ox (/ wo 2))
-            top (+ oy (/ wo 2))
-          ]
-
+            left (- centerx (/ viewwidth 2))
+            top (+ centery (/ viewwidth 2))
+            get-color (fn [^point p]
+                        (let [x (.x p)
+                              y (.y p)
+                              index (+ x (* y width))
+                              xc (double (+ (* (/ x width)  viewwidth) left))
+                              yc (double (- (* (/ y height) viewwidth) top))
+                              m (mandel (complex. xc yc )) ]
+                          (if (< m 0)
+                            [index (^int q/color 0 0 0)]
+                            [index (^int q/color (* m 255) (* m 128) 200)])))]
+      (time
         (doall
-          (pmap
-            #(doall
-                (map
-                  (fn [^point p]
-                    (let [
-                          x (.x p)
-                          y (.y p)
-                          row (* y width)
-                          xc (double (+ (* (/ x width)  wo) left))
-                          yc (double (- (* (/ y height) wo) top))
-                          m (mandel (complex. xc yc ))
-                          index (+ row x) ]
-                      (set-color m index pxls)))
-                  %))
+              (pmap #(doall
+                      (map (fn [[i c]] (aset image ^int i ^int c))
+                           (map get-color %)))
+                    (partition 8
+                      (for [y (range height) x (range width) ] (point. x y))))))
+      (println "drew")
+      (q/update-pixels)))
 
-            (partition 16
-              (for [y (range height) x (range width) ] (point. x y)))))
-        (println "drew")
-        (q/update-pixels))))
+(defn setup [])
 
 (q/defsketch example
   :title "image demo"
